5b2a81b421e4721dae73988771c052d08aa4f19d
grassblock <hi@imgb.space>
2025-02-08 15:32:51 +0800

    feat: migrate to KV storage

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { Buffer } from 'node:buffer'
-import { Hono } from 'hono'
-import { serveStatic } from 'hono/cloudflare-workers'
-import { html } from 'hono/html'
+import { Buffer } from 'node:buffer';
+import { Context, Hono } from 'hono';
+import { serveStatic } from 'hono/cloudflare-workers';
+import { html } from 'hono/html';
+// @ts-ignore
+import manifestJSON from '__STATIC_CONTENT_MANIFEST'
 
-import page from './page'
+import page from './page';
 
 export type Env = {
+  NAMESPACE: KVNamespace
   DOMAIN: string
-  UPSTASH_URL: string
-  UPSTASH_TOKEN: string
   PAD: number
   SELF: Fetcher
 }
@@ -20,16 +21,15 @@ app.get('/', page)
 const IMAGE_WIDTH = 45
 const IMAGE_HEIGHT = 100
 const image = async (
+  env: Env,
   val: string,
   idx: number,
   theme: string,
-  self: Fetcher
 ) => {
-  const url = `https://x/theme/${encodeURIComponent(
+  const url = `https://${env.DOMAIN}/theme/${encodeURIComponent(
     theme
-  )}/${encodeURIComponent(val)}.png`
-  const buf = await self
-    .fetch(url, {
+  )}/${encodeURIComponent(val)}.${theme.includes('gif') ? 'gif' : 'png'}`
+  const buf = await fetch(url, {
       cf: {
         cacheTtlByStatus: { '200-299': 86400, 404: 1, '500-599': 0 },
         cacheEverything: true,
@@ -37,7 +37,7 @@ const image = async (
     })
     .then((res) => res.arrayBuffer())
   const b64 = Buffer.from(buf).toString('base64')
-  const dataUrl = `data:image/gif;base64,${b64}`
+  const dataUrl = `data:image/${theme.includes('gif') ? 'gif' : 'png'};base64,${b64}`
 
   return html`
     <image
@@ -51,54 +51,46 @@ const image = async (
 }
 
 app.get('/get/:name', async (c) => {
-  const name = c.req.param('name')
-  const theme = c.req.query('theme') || 'moebooru'
-  const pad = c.req.query('pad') || c.env.PAD
+   const name = c.req.param('name')
+   const theme = c.req.query('theme') || 'moebooru'
+   const pad = Number(c.req.query('pad') || c.env.PAD)
 
-  let count
-  if (name !== '@demo') {
-    const { result } = await fetch(
-      `${c.env.UPSTASH_URL}/incr/${encodeURIComponent(name)}`,
-      {
-        headers: {
-          Authorization: `Bearer ${c.env.UPSTASH_TOKEN}`,
-        },
-      }
-    ).then((res) => res.json<{ result: number }>())
+   let count
+   if (name !== '@demo') {
+     const current  = Number(await c.env.NAMESPACE.get(name)) || 0
+     await c.env.NAMESPACE.put(name, String(current + 1))
+     count = await c.env.NAMESPACE.get(name) || '0'
+   } else {
+     count = '0123456789'
+   }
+   const chars = count.padStart(pad, '0').split('')
 
-    count = result.toString()
-  } else {
-    count = '0123456789'
-  }
+   return c.body(
+    await html`<?xml version="1.0" encoding="UTF-8"?>
+       <svg
+         width="${IMAGE_WIDTH * chars.length}"
+         height="${IMAGE_HEIGHT}"
+         version="1.1"
+         xmlns="http://www.w3.org/2000/svg"
+         xmlns:xlink="http://www.w3.org/1999/xlink"
+         style="image-rendering: pixelated;"
+       >
+         <title>Moco Count</title>
+         <g>
+           ${await Promise.all(
+             chars.map((x, idx) => image(c.env, x, idx, theme))
+           )}
+         </g>
+       </svg>`,
+     200,
+     {
+       'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate',
+       'Content-Type': 'image/svg+xml; charset=utf-8',
+       'X-Moco-Count': count,
+     }
+   )
+ })
 
-  const chars = count.padStart(pad, '0').split('')
-
-  return c.body(
-    html`<?xml version="1.0" encoding="UTF-8"?>
-      <svg
-        width="${IMAGE_WIDTH * chars.length}"
-        height="${IMAGE_HEIGHT}"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        style="image-rendering: pixelated;"
-      >
-        <title>Moco Count</title>
-        <g>
-          ${await Promise.all(
-            chars.map((x, idx) => image(x, idx, theme, c.env.SELF))
-          )}
-        </g>
-      </svg>`,
-    200,
-    {
-      'Cache-Control': 'max-age=0, no-cache, no-store, must-revalidate',
-      'Content-Type': 'image/svg+xml; charset=utf-8',
-      'X-Moco-Count': count,
-    }
-  )
-})
-
-app.get('/*', serveStatic({ root: './' }))
+app.get('/*', serveStatic({ manifest: "", root: './' }))
 
 export default app
